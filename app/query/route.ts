d9a18546cbf6901ac5691effe58cd12e94f544a3
Damion Betters <153688977+damionbetters@users.noreply.github.com>
2026-01-07 17:49:28 -0600

    push

--- app/query/route.ts
+++ app/query/route.ts
@@ -14,9 +14,13 @@ async function listInvoices() {
 }
 
 export async function GET() {
-  try {
-    return Response.json(await listInvoices());
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
+  return Response.json({
+    message:
+      "Uncomment this file and remove this line. You can delete this file when you are finished.",
+  });
+  // try {
+  // 	return Response.json(await listInvoices());
+  // } catch (error) {
+  // 	return Response.json({ error }, { status: 500 });
+  // }
 }
